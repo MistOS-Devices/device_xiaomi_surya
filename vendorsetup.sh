59638cea86365560a348836c21ce38e95d143d26
Zabuka_zuzu <zuzu950444@gmail.com>
2025-11-16 18:43:37 +0000

    Mistify Bringup 4.3

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-git clone https://github.com/crdroidandroid/android_kernel_xiaomi_surya.git -b 16.0 kernel/xiaomi/surya
-git clone https://gitlab.com/crdroidandroid/proprietary_vendor_xiaomi_surya.git -b 16.0 vendor/xiaomi/surya
+git clone --depth=1 https://github.com/crdroidandroid/android_kernel_xiaomi_surya.git -b 16.0 kernel/xiaomi/surya
+git clone --depth=1 https://gitlab.com/crdroidandroid/proprietary_vendor_xiaomi_surya.git -b 16.0 vendor/xiaomi/surya
 git clone https://github.com/Evolution-X-Devices/hardware_xiaomi.git -b bka hardware/xiaomi
